f99e5bc51cec4aa002ab710c8e121023bc690db9
daltojohnson <dalt.c.johnson@gmail.com>
2016-01-29 22:52:17 -0500

    adding some jest stuff

---
+++ __tests__/Calendar-test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import moment from 'moment';
+
+const Calendar = require('../src/js/Calendar');
+
+describe('Calendar', () => {
+   it('currently doesn\'t work with es6 imports?', () => {
+
+   };
+});
